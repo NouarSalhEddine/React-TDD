2409ed15b65704274be43cf0cb25fee68c0f9df9
nouar salheddine <nouarsalheddine@gmail.com>
2023-08-05 23:29:28 +0100

    add screen

--- src/Components/Addition.test.js
+++ src/Components/Addition.test.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import {screen, render, fireEvent } from '@testing-library/react'
 import Additiion from './Addition'
 
 it('render the correct result after addition', () => {
-  const { getByLabelText, getByText } = render(<Additiion />)
-  const input1 = getByLabelText('Number 1')
-  const input2 = getByLabelText('Number 2')
-  const addButton = getByText('Add') 
+  render(<Additiion />)
+  const input1 = screen.getByLabelText('Number 1')
+  const input2 = screen.getByLabelText('Number 2')
+  const addButton = screen.getByText('Add') 
   fireEvent.change(input1, { target: { value: '3' } })
   fireEvent.change(input2, { target: { value: '5' } })
   fireEvent.click(addButton)
-  const result = getByText('Result: 8')
+  const result = screen.getByText('Result: 8')
   expect(result).toBeInTheDocument()
 })
