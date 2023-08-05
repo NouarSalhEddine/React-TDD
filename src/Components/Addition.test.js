f19c6cb3a299ccd691ad193d40d0fe54f2ad2736
nouar salheddine <nouarsalheddine@gmail.com>
2023-08-05 23:20:00 +0100

    final test

--- src/Components/Addition.test.js
+++ src/Components/Addition.test.js
@@ -6,7 +6,7 @@ it('render the correct result after addition', () => {
   const { getByLabelText, getByText } = render(<Additiion />)
   const input1 = getByLabelText('Number 1')
   const input2 = getByLabelText('Number 2')
-  const addButton = getByText('Add')
+  const addButton = getByText('Add') 
   fireEvent.change(input1, { target: { value: '3' } })
   fireEvent.change(input2, { target: { value: '5' } })
   fireEvent.click(addButton)
