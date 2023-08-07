931614eed6d49768f6080f7791ad35e45150558b
nouar salheddine <nouarsalheddine@gmail.com>
2023-08-07 23:34:53 +0100

    add test addition

--- src/component/greet.test.tsx
+++ src/component/greet.test.tsx
@@ -1,9 +1,24 @@
-import { render, screen } from "@testing-library/react";
-import Greet from "./greet";
+import { fireEvent, render, screen } from "@testing-library/react"
+import Greet from "./greet"
 
-test('greet renders correctly', () => {
-  render(<Greet />)
-  const textElement = screen.getByText(/hello/i)
+test('greet render correctly',()=> {
+  render(<Greet/>)
+  const elementHello = screen.getByText(/hello/i)
+  expect(elementHello).toBeInTheDocument()
+})
+test('greet render with name', () => {
+  render(<Greet name='wingo'/>)
+  const textElement = screen.getByText('Hello wingo')
   expect(textElement).toBeInTheDocument()
-
+})
+test('addition two numbers', () => {
+  render(<Greet />)
+  const input1 = screen.getByPlaceholderText('input1')
+  const input2 = screen.getByPlaceholderText('input2')
+  const button = screen.getByText('add')
+  fireEvent.change(input1, { target: { value: '1' } })
+  fireEvent.change(input2, { target: { value: '2' } })
+  fireEvent.click(button)
+  const result = screen.getByText('3')
+  expect(result).toBeInTheDocument()
 })
--- src/component/greet.tsx
+++ src/component/greet.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
-const Greet = () => {
+type GreetProps = {
+  name?:String
+}
+const Greet = (props: GreetProps) => {
+  const [input1 , setInput1] = React.useState<Number>()
+  const [input2, setInput2] = React.useState<Number>()
+  const [result, setResult] = React.useState<Number>()
+  
+  const sum = () => {
+  setResult(Number(input1) + Number(input2)) 
+}
   return (
-    <div>Hello</div>
+    <>
+      <div>
+        Hello {props.name}
+        <input onChange={(e)=> setInput1(Number(e.target.value))} type="number" placeholder='input1' />
+        <input onChange={(e)=> setInput2(Number(e.target.value))} type="number" placeholder='input2' />
+        <button onClick={sum}>add</button>
+        <div>
+        {String(result)}
+
+        </div>
+
+      </div>
+    </>
   )
 }
 
