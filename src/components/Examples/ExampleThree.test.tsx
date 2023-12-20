db08346bd4151c1733372f798ca85564821dfcb1
nouarsalheddine <nouarsalheddine@gmail.com>
2023-12-20 20:50:19 +0100

    add option

--- src/components/Examples/ExampleThree.test.tsx
+++ src/components/Examples/ExampleThree.test.tsx
@@ -6,20 +6,19 @@ import ExampleThree from ".";
 
 describe("ExampleThree", () => {
   it("renders", () => {
-
     render(<ExampleThree />);
     const customElement = screen.getByTestId("custom-element");
     expect(customElement).toBeInTheDocument();
     const nameLabelElement = screen.getByLabelText("job location");
     expect(nameLabelElement).toBeInTheDocument();
 
-    const nameElement = screen.getByRole("textbox");
+    const nameElement = screen.getByRole("textbox", { name: "job location" });
     expect(nameElement).toBeInTheDocument();
 
     const jobLocationElement = screen.getByRole("combobox");
     expect(jobLocationElement).toBeInTheDocument();
 
-    const submitButtonElement = screen.getByRole("button");
+    const submitButtonElement = screen.getByRole("button", { name: "submit" });
     expect(submitButtonElement).toBeInTheDocument();
   });
 
