32d73436d6e0ac6ad62c17373b9d1ab92a2a4095
nouarsalheddine <nouarsalheddine@gmail.com>
2023-12-20 10:13:53 +0100

    keyboard interaction

--- src/components/Examples/counter/counter.test.tsx
+++ src/components/Examples/counter/counter.test.tsx
@@ -29,5 +29,21 @@ describe('Counter', () => {
     await userEvent.click(incrementButton);
     const countElement = screen.getByRole('heading');
     expect(countElement).toHaveTextContent('2');
-  } );
+  });
+  test('renders Counter component with count 0 after set button click',async () => {
+    render(<Counter />);
+    const setButton = screen.getByRole('button', { name: 'set' });
+    await userEvent.click(setButton);
+    const countElement = screen.getByRole('heading');
+    expect(countElement).toHaveTextContent('0');
+  });
+  test('renders Counter component with count 10 after set button click and input 10',async () => {
+    render(<Counter />);
+    const setButton = screen.getByRole('button', { name: 'set' });
+    const input = screen.getByRole('spinbutton');
+    await userEvent.type(input, '10');
+    await userEvent.click(setButton);
+    const countElement = screen.getByRole('heading');
+    expect(countElement).toHaveTextContent('10');
+  });
 });
